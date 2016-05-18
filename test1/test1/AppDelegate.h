c13a6177d2ead12e1a4d3e01893428e13bba1539
wwwying9 <yaoying@yaoyingdemac-mini.local>
2016-05-18 20:59:05 +0800

    init

---
+++ test1/test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by 姚英 on 16/5/18.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by 姚英 on 16/5/18.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
